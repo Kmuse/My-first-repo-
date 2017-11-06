0871cfbdfe93c2122b4af66ca078cd003ff7b01d
Kmuse <33422407+Kmuse@users.noreply.github.com>
2017-11-06 08:39:18 -0500

    Initial commit

---
+++ mad libs KM.py
@@ -0,0 +1,34 @@
+print("Write a verb ending in s")
+verbendingins1 = input()
+
+print("Write another verb")
+verb2 = input()
+
+print("Write a food")
+food3 = input()
+
+print("Write a color")
+color4 = input()
+
+print("Write a unrealistic job")
+job5 = input()
+
+print("write an animal")
+animal6 = input()
+
+print("write a weird job")
+weirdjob7 = input()
+
+print("write a time")
+time8 = input()
+
+print("write a transportation verb")
+transportationverb9 = input ()
+### MAD LIB ###
+
+print("Every morning Michaela" + verbendingins1 + "then she"
+      + verb2 + " down the stairs and eats her breakfast of" + food3 + " next she puts on her"
+      + color4 + " shirt. At michaelas" + job5 + " she needs to stay very hydrated so she drinks"
+      + animal6 + " juice. Although Michaela claims that is her job she secretley works an undercover"
+      + weirdjob7 + " when she leaves her house at exactly " + time8 + " she"
+      + transportationverb9 + "to work." )
